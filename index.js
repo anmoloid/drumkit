e10beee78be71a22de81f6e437e3d71d3bf655a2
Anmol Agarwal <anmolagarwal@Anmols-MacBook-Pro.local>
2023-03-23 21:59:04 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,65 @@
+
+//detecting button press
+
+for(var i=0;i<document.querySelectorAll(".drum").length;i++)
+{
+  
+document.querySelectorAll(".drum")[i].addEventListener("click", handleSound);
+function handleSound()
+{
+var buttonText=this.innerHTML;
+makeSound(buttonText)
+addButtonAnimation(buttonText)
+}
+}
+
+//detecting keyboard press
+document.addEventListener("keydown",function(event)
+{
+    makeSound(event.key)
+    addButtonAnimation(event.key)
+}
+)
+
+function makeSound(key)
+{
+    switch (key)
+  {
+    case "w":
+        var audio=new Audio('/Users/anmolagarwal/Documents/web development/Drum Kit Starting Files/sounds/crash.mp3');
+        audio.play();
+        break;
+    case "a":
+        var audio=new Audio('/Users/anmolagarwal/Documents/web development/Drum Kit Starting Files/sounds/kick-bass.mp3');
+        audio.play();
+        break;
+    case "s":
+        var audio=new Audio('/Users/anmolagarwal/Documents/web development/Drum Kit Starting Files/sounds/snare.mp3');
+        audio.play();
+        break;
+    case "d":
+        var audio=new Audio('/Users/anmolagarwal/Documents/web development/Drum Kit Starting Files/sounds/tom-1.mp3');
+        audio.play();
+        break;
+    case "j":
+        var audio=new Audio('/Users/anmolagarwal/Documents/web development/Drum Kit Starting Files/sounds/tom-2.mp3');
+        audio.play();
+        break;
+    case "k":
+        var audio=new Audio('/Users/anmolagarwal/Documents/web development/Drum Kit Starting Files/sounds/tom-3.mp3');
+        audio.play();
+        break;
+    case "l":
+        var audio=new Audio('/Users/anmolagarwal/Documents/web development/Drum Kit Starting Files/sounds/tom-4.mp3');
+        audio.play();
+        break;
+  }
+}
+function addButtonAnimation (currentKey)
+{
+ var activebutton=document.querySelector("."+currentKey)
+ activebutton.classList.add("pressed")
+ setTimeout(function(){
+    activebutton.classList.remove("pressed")
+ },100)
+}
